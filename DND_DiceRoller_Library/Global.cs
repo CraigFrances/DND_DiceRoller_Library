63b741dbdb665b3c73ab9987a0a055c61398dc3b
Craig Franceschetti <craigfranceschetti@gmail.com>
2021-09-29 08:16:36 -0500

    Creating global variables in the library

---
+++ DND_DiceRoller_Library/Global.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DND_DiceRoller_Library
+{
+    /// <summary>
+    /// Global variables for the dice roller application.
+    /// </summary>
+    public static class Global
+    {
+        /// <summary>
+        /// Global variable that collects user input for the dice they want.
+        /// </summary>
+        public static string RollSelection
+        {
+            get
+            {
+                return Console.ReadLine().ToUpper();
+            }
+        }
+    }
+}
